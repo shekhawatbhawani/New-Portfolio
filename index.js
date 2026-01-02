3821efe25cd5c171406cf74dfbc85ab85cbb3124
Bhawani Singh Shekhawat <shekhawatbhwanisingh49@gmail.com>
2026-01-02 19:26:40 +0530

    my new portfolio

---
+++ index.js
@@ -0,0 +1,25 @@
+console.log("Jai Shree Ram")
+
+
+let button = document.querySelector(".menu-icon")
+
+let toggleNav = document.querySelector("#toggle-nav")
+
+console.log(button)
+
+
+button.addEventListener("click", function(){
+
+	let ans = toggleNav.classList.toggle("active")
+
+    console.log("Jai Shree Ram");
+
+    console.log(ans);
+    
+    
+});
+
+
+
+
+
